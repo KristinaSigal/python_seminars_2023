07668eaa6a1d80c1d226f120339feeea7c402bff
kristina.sigal <kulrikh@gmail.com>
2023-07-21 23:55:36 +0300

    Add new branch with tasks from ith seminar

---
+++ s2_script_1.py
@@ -0,0 +1,13 @@
+"""
+Задание №1
+Создайте несколько переменных разных типов.
+Проверьте к какому типу относятся созданные переменные
+"""
+
+data_list = [4, 3.1415, True, None, 'hello']
+for el in data_list:
+    print(type(el))
+
+data_list = [4, 3.1415, True, None, 'hello']
+for i in range(len(data_list)):
+    print(i, type(data_list[i]))
