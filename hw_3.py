87e903bc33ac541ac52aaf8af5f5a6c659a1f69d
kristina.sigal <kulrikh@gmail.com>
2023-08-03 19:51:24 +0300

    Add hw_3

--- hw_3.py
+++ hw_3.py
@@ -5,15 +5,13 @@
 """
 
 
-# def del_duplicates(lst):
-#     return list(set([x for x in lst if lst.count(x) > 1]))
-
 def del_duplicates(lst):
-    res_list=[]
+    res_list = []
     for x in lst:
         if lst.count(x) > 1 and res_list.count(x) == 0:
             res_list.append(x)
     return res_list
+
 
 lst = [1, 1, 6, 3, 2, 3, 4, 4, 2, 2, 5, 5, 6]
 print(del_duplicates(lst))
